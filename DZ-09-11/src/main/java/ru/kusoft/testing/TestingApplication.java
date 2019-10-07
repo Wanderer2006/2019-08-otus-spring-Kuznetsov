fe0912cc0a4408e655e5976b161a0b558258b16e
kuznetsovap <kuznetsovap@elfin.tech>
2019-10-07 23:53:39 +0300

    Delete annotation

--- DZ-09-11/src/main/java/ru/kusoft/testing/TestingApplication.java
+++ DZ-09-11/src/main/java/ru/kusoft/testing/TestingApplication.java
@@ -1,11 +1,9 @@
 package ru.kusoft.testing;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class TestingApplication {
 
 	public static void main(String[] args) {
